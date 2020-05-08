4d42832f8abb4c493cb4fcf397e850562f53bc81
Robert Roose <robert@shivercube.com>
2020-05-08 13:59:53 -0300

    Container only has curl installed by default

--- files/usr/local/bin/custom-entrypoint.sh
+++ files/usr/local/bin/custom-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-S3_BUCKET=$(wget -q -O - http://vault.in.okinta.ge:7020/api/kv/nuget_bucket)
+S3_BUCKET=$(curl -s http://vault.in.okinta.ge:7020/api/kv/nuget_bucket)
 if [ -z "$S3_BUCKET" ]; then
     echo "Could not obtain S3 bucket from Vault" >&2
     exit 1
